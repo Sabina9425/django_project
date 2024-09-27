3a81ec9a8d4791fac98d6b8acdda6301b5aec9f1
Sabina9425 <visapa@icloud.com>
2024-09-27 21:39:17 +0500

    add new users app; create User model

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'catalog'
+    'catalog',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -91,3 +92,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
